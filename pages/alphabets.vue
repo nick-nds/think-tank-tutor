a072566456e0eeffe81c869267a7b52882742d16
Niku Nitin <niku.nitin@codeclouds.in>
2023-02-14 10:33:32 +0530

    Add - titles

--- pages/alphabets.vue
+++ pages/alphabets.vue
@@ -1,3 +1,11 @@
+<script setup>
+import { inject } from 'vue'
+
+const title = inject('preTitle')
+title.value = 'Alphabets'
+
+</script>
+
 <template>
 </template>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,10 @@
+<script setup>
+import { inject } from 'vue'
+
+const title = inject('preTitle')
+title.value = 'Dashboard'
+
+</script>
 <template>
 <h1>Page</h1>
 </template>
